0d84cb1c9c4775c75e0d2b8a3f17b89035a99ca3
cmscms419 <alstjr123qwe@gmail.com>
2025-02-03 19:07:34 +0900

    RenderDoc shader Debug 가능

---
+++ shader/compile_debug.bat
@@ -0,0 +1,3 @@
+C:/VulkanSDK/1.4.304.0/Bin/glslangValidator.exe -e main -gVS -V -o vert.spv shader.vert
+C:/VulkanSDK/1.4.304.0/Bin/glslangValidator.exe -e main -gVS -V -o frag.spv shader.frag
+pause
