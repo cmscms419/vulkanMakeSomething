65dfab37f4ce896e0d9e0aef667dd679b07a03e5
cmscms419 <alstjr123qwe@gmail.com>
2026-05-18 14:41:17 +0900

    빠진부분 추가

--- app/source/sampleDemo/moveDemo.h
+++ app/source/sampleDemo/moveDemo.h
@@ -3,8 +3,25 @@
 
 #include "Application.h"
 
-namespace vkengine {
+namespace vkengine
+{
+    class DemoApplication : public Application
+    {
+    public:
+        DemoApplication(cString root_path);
+        ~DemoApplication();
 
+        virtual void update() override;
+    private:
+        cFloat FindYGround(const cVec3& position);
+        void resolveWallCollisions(cVec3& position);
+
+        cVec3 velocity;
+        cBool isGrounded;
+        cBool isMoving;
+        cBool isSpeedUp;
+        cFloat currentFacingYaw;
+    };
 }
 
 #endif // RESUME_DEMO_H_
